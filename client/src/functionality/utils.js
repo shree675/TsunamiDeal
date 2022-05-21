ee8ce634b5db316abcb1b5346a53b95870edf7df
Shreetesh M <shreeteshm37@gmail.com>
2022-05-21 20:45:14 +0530

    changes for report

--- client/src/functionality/utils.js
+++ client/src/functionality/utils.js
@@ -12,11 +12,5 @@ export async function queryExchange(queryPassed) {
     console.log(error);
   });
   const json = await response.json();
-  //console.log(json);
   return json;
-  // for select, data.fields gives the column details,
-  // data.rows gives the list of rows, each list item
-  // is an object that contains column name as the key and
-  // cell value as the value
-  //console.log(data);
 }
